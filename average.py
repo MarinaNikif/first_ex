a8dc7bf2c8f633e7ce83924545e6186e4261cacd
MarinaNikif <117200047+MarinaNikif@users.noreply.github.com>
2024-04-13 17:42:54 +0300

    Update average.py

--- average.py
+++ average.py
@@ -4,8 +4,7 @@ def calculate_and_display_average_price(data):
 
 
 
-def notify_if_strong_fluctuations(data):
-    threshold = 5  # Пример порога в 5%
+def notify_if_strong_fluctuations(data, threshold):
     max_price = data['Close'].max()
     min_price = data['Close'].min()
     price_diff = (max_price - min_price) / min_price * 100
@@ -14,6 +13,3 @@ def notify_if_strong_fluctuations(data):
         print(f"Цена акций колебалась более чем на {threshold}% за период.")
     else:
         print("Колебания цены акций не превысили заданный порог.")
-
-
-
