ed4a102f4f4460d9356c6150179a6871432783a5
MarinaNikif <117200047+MarinaNikif@users.noreply.github.com>
2024-04-13 17:31:44 +0300

    Add files via upload

---
+++ average.py
@@ -0,0 +1,19 @@
+def calculate_and_display_average_price(data):
+    average_price = data['Close'].mean()
+    print(f"Средняя цена закрытия акций за заданный период: {average_price}")
+
+
+
+def notify_if_strong_fluctuations(data):
+    threshold = 5  # Пример порога в 5%
+    max_price = data['Close'].max()
+    min_price = data['Close'].min()
+    price_diff = (max_price - min_price) / min_price * 100
+
+    if price_diff > threshold:
+        print(f"Цена акций колебалась более чем на {threshold}% за период.")
+    else:
+        print("Колебания цены акций не превысили заданный порог.")
+
+
+
